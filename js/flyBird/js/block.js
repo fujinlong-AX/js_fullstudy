8388f9c81beb2bc37709451274f8ad5fdf1bff7e
fujinlong-AX <1160893962@qq.com>
2020-07-15 22:55:56 +0800

    提交

--- js/flyBird/js/block.js
+++ js/flyBird/js/block.js
@@ -25,7 +25,22 @@ function Block() {
         this.upDivWrap.appendChild(upDiv1)
         this.upDivWrap.appendChild(upDiv2)   //生成上方管道
 
+        var downDiv1 = this.createDiv('url(img/down_pipe.png)', '60px')
+        var downDiv2 = this.createDiv('url(img/down_mod.png)', this.downHeight + 'px')
+        this.downDivWrap = this.createDiv(null, null, 'absolute', '450px', 363 - this.downHeight + 'px')
+        this.downDivWrap.appendChild(downDiv1)
+        this.downDivWrap.appendChild(downDiv2) 
+
+
+
 
         jsWrapBg.appendChild(this.upDivWrap)
+        jsWrapBg.appendChild(this.downDivWrap)
+    }
+
+    this.moveBlock = function() {
+        this.upDivWrap.style.left = this.upDivWrap.offsetLeft - 3 +'px'
+        this.downDivWrap.style.left = this.downDivWrap.offsetLeft - 3 +'px'
+        
     }
 }
