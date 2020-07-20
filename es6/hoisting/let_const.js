64339344c773edcdc702cda53f4e48bd65d282d4
fujinlong-AX <1160893962@qq.com>
2020-07-20 12:10:33 +0800

    tijiao

---
+++ es6/hoisting/let_const.js
@@ -0,0 +1,22 @@
+// for(let i = 0; i < 3; i++)  {  //父作用域
+//     let i = 'abc'        // 子作用域
+// }
+
+
+
+// 暂时性死区
+// var tmp = 123
+
+// if(true){
+//     tmp = 'abc'
+// }
+
+
+var a ='a'
+var a = 'aa'
+console.log(a)
+
+
+let a ='a'
+let a = 'aa'
+console.log(a)
