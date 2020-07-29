22020abf24758db1e91c44ff419cd9f74d8411e1
fujinlong-AX <1160893962@qq.com>
2020-07-29 12:08:41 +0800

    douban

--- wxapp/doubanMovie/component/ratingStar/ratingStar.js
+++ wxapp/doubanMovie/component/ratingStar/ratingStar.js
@@ -4,20 +4,48 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    score: {
+      type:Number,
+      value: 0
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    arrayUrl: []
   },
 
+
+  lifetimes:{
+      attached: function () {
+      let score = this.data.score
+      console.log(score)
+      let scoreInt = parseInt(score)
+      let arrayUrl = this.data.arrayUrl
+      for(let i = 0; i < scoreInt; i++){
+        arrayUrl.push('../../assets/img/rating_star_small_on.png')
+      } 
+      if(score === scoreInt){
+        for(let i = 0; i < (5-scoreInt);i++){
+          arrayUrl.push('../../assets/img/rating_star_small_off.png')
+        }
+      }else{
+        arrayUrl.push('../../assets/img/rating_star_small_half.png')
+        for(let i = 0; i < (4 - scoreInt);i++){
+          arrayUrl.push('../../assets/img/rating_star_small_off.png')
+        }
+      }
+      this.setData({
+        arrayUrl
+      })
+    }
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    
   }
 })
--- wxapp/doubanMovie/pages/home/home.js
+++ wxapp/doubanMovie/pages/home/home.js
@@ -11,10 +11,54 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (options) { //打开页面生命周期就会自动执行
+    this.getCity((city) => {
+      this.loadData(0, {
+        city: city,
+        apikey:'0df993c66c0c636e29ecbb5344252a4a'
+      })
+    })
   },
 
+  loadData(idx, params) {
+    console.log(params)
+    let url = wx.db.url('/v2/movie/in_theaters')
+    wx.request({
+      url: url,
+      data: params,
+      header: {'content-type': 'json'},
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+
+  getCity(succeed) {
+    // 拿到当前所在的城市名称
+    // 先拿到当前所在区域的经纬度(小程序自带的api)
+    wx.getLocation({
+      success: function (res) {
+        wx.request({ // 往百度地图发起请求
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          data: {
+            ak: 'uXDG6lrpKFg36aUkGnzzcrizuPfTdcVH',
+            output: 'json',
+            coordtype: 'wgs84ll',
+            location: `${res.latitude},${res.longitude}`
+          },
+          success: (res) => {
+            console.log(res);
+            let city = res.data.result.addressComponent.city
+            succeed && succeed(city) //这是啥？
+          },
+          fail: () => {
+            console.log('获取城市失败');
+          }
+        })
+      },
+      fail: () => {},
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
