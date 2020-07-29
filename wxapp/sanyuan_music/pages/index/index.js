2308818b222c56ae53566415140e7c4b870cb2e8
fujinlong-AX <1160893962@qq.com>
2020-07-29 17:07:17 +0800

    tijiao

---
+++ wxapp/sanyuan_music/pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    bannerList: [],
+    recommendList:[],
+    isRecommendListLoading: true
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+   wx.request({
+     // 接口地址
+     url:'http://47.98.159.95/m-api/banner',
+     success:(res) => {
+      //  if(res.)
+       console.log(res);
+       this.setData({
+        bannerList: res.data.banners,
+        
+       })
+     }
+   })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ wxapp/sanyuan_music/readme.md
@@ -0,0 +1,13 @@
+- 数据来源
+  1. 有来源接口
+    - banner
+    - personalized
+  2. 假数据  mocker.js
+  data: {
+    list: [
+      {
+        key: val,
+
+      }
+    ]
+  }  
