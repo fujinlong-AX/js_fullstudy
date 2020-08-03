dbbebb1c3ea911256cbdbde2a7867a308204303b
fujinlong-AX <1160893962@qq.com>
2020-08-03 11:59:15 +0800

    scope

---
+++ mooc/js/1.js
@@ -0,0 +1,6 @@
+var a;
+if(a=0){
+console.log('ok');
+}else{
+console.log('shit');
+} 
---
+++ scope/lexocal-scope/1.js
@@ -0,0 +1,20 @@
+var a = 10   //等于 window.a = 10
+function foo(a) {
+  var b = a +2
+
+  function bar(c) {
+    console.log(window.a, b, c);
+  }
+
+  bar(b * 3)
+}
+// bar(5)
+foo(2)
+console.log(b);
+
+
+// 全局作用域下面只有一个 foo
+// foo作用域下面有 a，b，bar
+// bar作用域下有 c
+
+
