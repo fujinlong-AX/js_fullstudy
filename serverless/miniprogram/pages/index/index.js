3910848b6f022deca24d88bfd0c04f58a1cb049a
fujinlong-AX <1160893962@qq.com>
2020-08-10 23:03:22 +0800

    wxapp

--- serverless/miniprogram/pages/index/index.js
+++ serverless/miniprogram/pages/index/index.js
@@ -6,6 +6,29 @@ const db = wx.cloud.database();
 const productsCollection = 
   db.collection('products');
 Page({
+  uploadAvatar() {
+    // 上传图片 选择上传类型
+    // 图片
+    // 上传到云服务器
+    // 硬件
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      success: res => {
+        // console.log(res.tempFilePaths);
+        for(let file of res.tempFilePaths) {
+          let currentFileName = Math.floor(Math.random() * 1000000).toString() + '.png'
+          wx.cloud.uploadFile({
+            cloudPath: currentFileName,
+            filePath,
+            success: res => {
+              console.log(res);
+            }
+          })
+        }
+      }
+    })
+  },
   data: {
     products: []
   },
