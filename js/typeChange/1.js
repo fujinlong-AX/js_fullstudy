eac275a096e5419d1732bd4b34fea882f09b0e8c
fujinlong-AX <1160893962@qq.com>
2020-08-12 14:29:00 +0800

    类型转换

---
+++ js/typeChange/1.js
@@ -0,0 +1,29 @@
+// String Boolean Number Undefined Null Object
+
+// var a = 1
+// var b = new Number(1)
+
+// 原始值转布尔
+// console.log(Boolean());
+// console.log(Boolean(true));
+// console.log(Boolean(false));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(+0));
+// console.log(Boolean(-0));
+// console.log(Boolean(1));
+// console.log(Boolean(-1));
+// console.log(Boolean(""));
+// console.log(Boolean("123"));
+// console.log(Boolean(NaN));
+
+
+// console.log(Number());
+// console.log(Number(undefined));
+// console.log(Number(null));
+// console.log(Number(false));
+// console.log(Number(true));
+// console.log(Number('123'));
+// console.log(Number('0x11'));
+
+
