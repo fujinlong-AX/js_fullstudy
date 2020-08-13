0652256baae8bf9a42634f2305de54b3924a9707
fujinlong-AX <1160893962@qq.com>
2020-08-13 20:59:51 +0800

    s

---
+++ wxapp/vant-cloud/miniprogram/pages/index/index.js
@@ -0,0 +1,129 @@
+// miniprogram/pages/index/index.js
+import Notify from '../../miniprogram_npm/@vant/weapp/notify/notify';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    newGroupModal: false,
+    groupName: ''
+  },
+
+  showNewGroupModal() {
+    this.setData({
+      newGroupModal: true
+    })
+  },
+  closeDialog() {
+    this.setData({
+      newGroupModal: false
+    })
+  },
+  createGroup() {
+    const self = this
+    if (this.data.groupName === '') {
+      // 出现notify
+      Notify({
+        message: '起个名字吧',
+        duration: 2000,
+        selector: '#notify-selector',
+        background: '#dc3545'
+      });
+      self.selectComponent("#new-group-modal").stopLoading()
+      return
+    }
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'createGroup',
+      data: {
+        groupName: self.data.groupName
+      },
+      success(res) {
+        console.log(res)
+        self.setData({
+          groupName: '',
+          newGroupModal: false
+        })
+        // 出现notify
+        Notify({
+          message: '新建成功',
+          duration: 2000,
+          selector: '#notify-selector',
+          background: '#28a745'
+        });
+        setTimeout(() => {
+          wx.switchTab({
+            url: '/pages/group/group'
+          })
+        }, 2000)
+      },
+      fail(error) {
+        console.log(error)
+      }
+    })
+  },
+
+  onGroupNameChange(event) {
+    console.log(event)
+    this.setData({
+      groupName: event.detail
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
