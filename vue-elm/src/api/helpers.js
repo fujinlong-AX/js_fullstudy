d65fabff8bb0093d6f54f93ef44f011f0d823642
fujinlong-AX <1160893962@qq.com>
2020-09-01 18:38:28 +0800

    vue-elm

---
+++ vue-elm/src/api/helpers.js
@@ -0,0 +1,21 @@
+import axios from 'axios';//  ???
+
+const baseUrl = 'http://ustbhuangyi.com/sell/';
+// ???
+export function get(url) {
+  return function (params = {}) {
+    return axios.get(baseUrl + url, {
+      params
+    }).then((res) => {
+      const {errno, data} = res.data
+      //上面这一句就等于下面这两行，，，es6解构的内容，对象的解构
+      //const errno = res.data.errno
+      //const data = res.data.data
+      if (errno === 0) {
+        return data
+      }
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+}
--- vue-elm/src/components/v-header/v-header.vue
+++ vue-elm/src/components/v-header/v-header.vue
@@ -28,5 +28,51 @@
 export default {};
 </script>
 
-<style>
+<style lang="stylus">
+ @import "../../common/stylus/variable.styl";
+ @import "../../common/stylus/mixin.styl";
+ .header
+  position relative
+  overflow hidden
+  color $color-white
+  background $color-background-ss
+  .content-wrapper
+    position relative
+    display flex
+    align-items center
+    padding 24px 12px 18px 24px
+    .avatar
+      flex 0 0 64px
+      width 64px
+      margin-right 16px
+      img
+        border-radius 2px
+    .content
+      flex 1
+      .title
+        display flex
+        align-items center
+        margin-bottom 8px
+        .brand
+          width 30px
+          height 18px
+          bg-image('brand')
+          background-size 30px 18px
+          background-repeat no-repeat
+        .name
+          margin-left 6px
+          font-size $fontsize-large
+          font-weight bold
+      .description
+        margin-bottom 8px
+        line-height 12px
+        font-size $fontsize-small
+      .support
+        display flex
+        align-items center
+        &-ico
+          margin-right 4px
+        .text
+          line-height 12px
+          font-size $fontsize-small-s
 </style>
