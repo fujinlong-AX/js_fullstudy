28fb56583c2b3a670783c3bfa4873b50dd6a26ce
fujinlong-AX <1160893962@qq.com>
2020-11-20 16:53:07 +0800

    myvue

---
+++ vue-ssr/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Com></Com>
+  </div>
+</template>
+
+<script>
+import Com from './Com.vue'
+export default {
+  components: {
+    Com
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-ssr/src/entry-client.js
@@ -0,0 +1,5 @@
+import { create } from './app.js'
+let { app} = createApp()
+app.mount('#app')
+
+ // 渲染在浏览器上
